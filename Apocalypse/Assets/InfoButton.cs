0665c895b1e612e3c2998a50eec81fc18109ea06
Ukdam <90294651+Ukdam@users.noreply.github.com>
2022-11-30 05:28:35 +0900

    asd

--- Apocalypse/Assets/InfoButton.cs
+++ Apocalypse/Assets/InfoButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InfoButton : MonoBehaviour
 {
-    public GameObject Infopn;
+    public GameObject Infopn, Infopn2, Infopn3;
 
     public void InfoOpen()
     {
@@ -14,5 +14,43 @@ public class InfoButton : MonoBehaviour
     public void InfoClose()
     {
         Infopn.SetActive(false);
+    }
+
+    public void InfoClose2()
+    {
+        Infopn2.SetActive(false);
+    }
+
+    public void InfoClose3()
+    {
+        Infopn3.SetActive(false);
+    }
+
+    public void Nextpage()
+    {
+        if(Infopn.activeSelf == true)
+        {
+            Infopn.SetActive(false);
+            Infopn2.SetActive(true);
+        }
+        else if(Infopn2.activeSelf == true)
+        {
+            Infopn2.SetActive(false);
+            Infopn3.SetActive(true);
+        }
+    }
+
+    public void Lastpage()
+    {
+        if(Infopn2.activeSelf == true)
+        {
+            Infopn.SetActive(true);
+            Infopn2.SetActive(false);
+        }
+        else if(Infopn3.activeSelf == true)
+        {
+            Infopn2.SetActive(true);
+            Infopn3.SetActive(false);
+        }
     }
 }
